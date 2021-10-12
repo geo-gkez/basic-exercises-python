15885fd24b18d616bad8f559f21394e31b1c1be5
geo-gkez <georgegezeris@gmail.com>
2021-10-12 11:39:33 +0000

    example count days

---
+++ exampleCarWashEveryDay.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+def washCar(money:int):
+    today = date.today()
+    day=int(today.strftime("%d"))
+    
+    countDay=0
+    while money >=1:
+        if day%2 == 0:
+            money=money-1
+           
+        else:
+            if money>=2:
+                money=money-2
+            else:
+                break    
+
+        print('count: {}, money: {} '.format(day,money))    
+        day+=1
+        countDay+=1
+        
+        
+
+    print('you can wash your car for {} days on the row.'.format(countDay))
+   
+
+washCar(5);
